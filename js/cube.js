08afc44ddffcb48a921ea2b6d73183b0d5d20f50
elinsia <elinsia7@gmail.com>
2017-06-21 18:30:31 +0300

    -cube finally moves on axis -fixed overlay on photo-cart

--- js/cube.js
+++ js/cube.js
@@ -3,91 +3,147 @@ $(function () {
     var deg = 0;
     var IntervalId = 0;
     var moveId = 0;
-    var action = false;
+    var active = false;
     var pixelCountX = 0;
     var pixelCountY = 0;
 
-    function rotateBox(deg) {
-        $('.cube-container').css('transform', 'rotateX('+ deg + 'deg) ' +
+    function rotateBox() {
+       $('.cube-container').css('transform', 'translateY('+pixelCountY+'px)' + 'translateX('+pixelCountX+'px) rotateX('+ (deg) + 'deg)'+
             'rotateY(' + deg + 'deg)');
     }
-    var rotateUp = function () {
-        return $('.cube-container').css('transform', 'rotateX('+ deg + 'deg)');
-    };
-    var rotateDown = function () {
-        return $('.cube-container').css('transform', 'rotateX('+ (-deg) + 'deg)');
-    };
-    var rotateLeft = function () {
-        return $('.cube-container').css('transform', 'rotateY('+ (deg) + 'deg)');
-    };
-    var rotateRight = function () {
-        return $('.cube-container').css('transform', 'rotateY('+ (-deg) + 'deg)');
-    };
-    function moveOnClick(move)
-    {
-        var currentDeg = deg;
-        IntervalId = setInterval(function () {
-            deg++;
-            move();
-            if((deg-currentDeg)>=90){
-                clearInterval(IntervalId);
-            }
-
-        }, 10);
-    }
     $('.btn.start').click(function () {
-        if(!action){
-        IntervalId = setInterval(function () {
-            deg++;
-            rotateBox(deg);
-            if(deg==360){
-                deg=0;
-            }
-        }, 15)
+        if(!active){
+            IntervalId = setInterval(function () {
+                deg++;
+                rotateBox();
+                if(deg==360){
+                    deg=0;
+                }
+            }, 10)
         }
-        action = true;
+        active = true;
     });
     $('.btn.pause').click(function () {
         clearInterval(IntervalId);
-        action = false;
+        clearInterval(moveId);
+        active = false;
     });
     $('.btn.stop').click(function () {
         deg = 0;
-        rotateBox(deg);
+        pixelCountY=0;
+        pixelCountX=0;
         clearInterval(IntervalId);
-        action = false;
+        clearInterval(moveId);
+        rotateBox();
+        active = false;
     });
 
-    $('.btn.up').click(function () {
-       //moveOnClick(rotateUp);
-        //console.log($('.cube-container').css('transform','translateX()'));
-        var lastY = pixelCountY;
-        moveId = setInterval(function () {
-            pixelCountY--;
-            $('.cube-container').css('transform','translateY('+pixelCountY+'px)');
-
-            if(lastY-pixelCountY>=100){
-               clearInterval(moveId);
-            }
-            console.log(document.getElementById('cube-container').style('transform', 'translateX'));
-
-        })
-    });
     $('.btn.down').click(function () {
+        clearInterval(moveId);
         var lastY = pixelCountY;
-        moveId = setInterval(function () {
-            pixelCountY++;
-            $('.cube-container').css('transform','translateY('+pixelCountY+'px)');
+        if(!active){
+            moveId = setInterval(function () {
+                pixelCountY++;
+                rotateBox();
+                if(pixelCountY-lastY>=100){
+                    clearInterval(moveId);
+                }
+            })
+        }else{
+            moveId = setInterval(function () {
+                clearInterval(IntervalId);
+                deg++;
+                if(pixelCountY-lastY<=300){
+                    pixelCountY++;
+                }
+                rotateBox()
+                if(deg==360){
+                    deg=0;
+                }
+            }, 10)
+        }
 
-            if(pixelCountY-lastY>=100){
-                clearInterval(moveId);
-            }
-        })
+    });
+    $('.btn.up').click(function () {
+        clearInterval(moveId);
+        var lastY = pixelCountY;
+        if(!active){
+            moveId = setInterval(function () {
+                pixelCountY--;
+                rotateBox();
+                if(lastY-pixelCountY>=100){
+                    clearInterval(moveId);
+                }
+
+            })
+        }else{
+            moveId = setInterval(function () {
+                clearInterval(IntervalId);
+                deg++;
+                rotateBox();
+
+                if(lastY-pixelCountY<=300){
+                    pixelCountY--;
+                }
+                if(deg==360){
+                    deg=0;
+                }
+            }, 10)
+        }
     });
     $('.btn.left').click(function () {
-        moveOnClick(rotateLeft);
+        clearInterval(moveId);
+        var lastX = pixelCountX;
+        if(!active){
+            moveId = setInterval(function () {
+                pixelCountX--;
+                rotateBox();
+
+                if(lastX-pixelCountX>=100){
+                    clearInterval(moveId);
+                }
+
+            })
+        }else {
+            moveId = setInterval(function () {
+                clearInterval(IntervalId)
+                deg++;
+                if (lastX - pixelCountX <= 300) {
+                    pixelCountX--;
+                }
+                rotateBox();
+
+                if (deg == 360) {
+                    deg = 0;
+                }
+            }, 10)
+        }
     });
     $('.btn.right').click(function () {
-        moveOnClick(rotateRight);
+        clearInterval(moveId);
+        var lastX = pixelCountX;
+        if(!active){
+            moveId = setInterval(function () {
+                pixelCountX++;
+                rotateBox();
+
+                if(pixelCountX-lastX>=100){
+                    clearInterval(moveId);
+                }
+
+            })
+        }else {
+            moveId = setInterval(function () {
+                clearInterval(IntervalId)
+                deg++;
+                if (pixelCountX - lastX <= 300) {
+                    pixelCountX++;
+                }
+                rotateBox();
+                if (deg == 360) {
+                    deg = 0;
+                }
+            }, 10)
+        }
     })
 });
