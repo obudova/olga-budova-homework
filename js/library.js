93ed4080256023896e21a705b25e932de54f391e
elinsia <elinsia7@gmail.com>
2017-06-27 16:04:29 +0300

    accordion finished

--- js/library.js
+++ js/library.js
@@ -2,7 +2,7 @@ $('.dropdown span').click(function () {
     $(this).parent().toggleClass('show');
 });
 
-$('.accordion .item .item__title').click(function () {
+$('.accordion.multi-target .item .item__title').click(function () {
     if($(this).parent().hasClass('expanded')){
         $(this).parent().removeClass('expanded');
         $(this).parent().children('.item__content').hide(300);
@@ -12,4 +12,26 @@ $('.accordion .item .item__title').click(function () {
         $(this).parent().children('.item__content').show(300);
     }
 
+});
+$('.accordion.default .item .item__title').click(function () {
+
+    if($(this).parent().hasClass('expanded')){
+        $(this).parent().removeClass('expanded');
+        $(this).parent().children('.item__content').hide(300);
+
+    }else{
+        $('.accordion.default .item').removeClass('expanded');
+        $('.accordion.default .item .item__content').hide(300);
+        $(this).parent().addClass('expanded');
+        $(this).parent().children('.item__content').show(300);
+    }
+
+});
+$('.with-sub-list .sub-list .list-item').click(function () {
+    if($(this).hasClass('chosen')){
+        $(this).removeClass('chosen');
+    }else{
+        $('.with-sub-list .sub-list .list-item').removeClass('chosen');
+        $(this).addClass('chosen');
+    }
 });
