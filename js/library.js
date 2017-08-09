1d809654db51e4b10f83687ece7557805b828ae6
Olga <olgabudova55@gmail.com>
2017-08-09 08:11:56 +0300

    search added

--- js/library.js
+++ js/library.js
@@ -35,10 +35,68 @@ $('.with-sub-list .sub-list .list-item').click(function () {
         $(this).addClass('chosen');
     }
 });
-$('.tab-container .tabs .tab').click(function () {
+$('.tab-container > .tabs .tab').click(function () {
     var target = $(this).attr('data-tab');
-    $('.tab-container .tab').removeClass('active');
+    $('.tab-container > .tabs .tab').removeClass('active');
     $('.tab-container .content').removeClass('active');
     $(this).addClass('active');
     $(target).addClass('active');
 });
+$('.tab-container  .tabs .tab').click(function () {
+    $('.tab-container .tabs-variation .tab').removeClass('active');
+    $(this).addClass('active');
+});
+var sliderWidth = $('.slider__container').width();
+var count = 0;
+var distance = 0;
+var slideCount = $('.slider__container .slider__row .slide').length-1;
+$('.slider__container .slider__row').css('width', ((slideCount+1)*sliderWidth)+'px');
+function moveSlide() {
+    distance=sliderWidth*count*(-1);
+    $('.slider__container .slider__row').css('transform','translate('+ distance +'px)');
+}
+$('.arrow.right').click(function () {
+    count++;
+    if(count > slideCount){
+        count=0;
+    }
+   moveSlide();
+
+});
+$('.arrow.left').click(function () {
+    count--;
+    if(count < 0){
+        count= slideCount;
+    }
+    moveSlide();
+});
+
+const btn = document.querySelector('.search-button'),
+        searchContainer = document.querySelector('.search__container'),
+        closeSearch = document.querySelector('.search-close-button'),
+        inputSearch = document.getElementById('search-input');
+console.log(btn);
+btn.addEventListener('click', onSearchBtnClick);
+closeSearch.addEventListener('click', onCloseSearch);
+inputSearch.addEventListener('focus', onInputFocus);
+inputSearch.addEventListener('blur', onInputBlur);
+inputSearch.addEventListener('input', onTextInput);
+function onSearchBtnClick(e){
+    searchContainer.classList.add('search-active');
+}
+function onCloseSearch() {
+    searchContainer.classList.remove('search-active');
+}
+function onInputFocus() {
+    searchContainer.classList.add('is-focused');
+}
+function onInputBlur() {
+    searchContainer.classList.remove('is-focused');
+}
+function onTextInput() {
+    if(this.value.length>0){
+        searchContainer.classList.add('text-entered');
+    }else {
+        searchContainer.classList.remove('text-entered');
+    }
+}
